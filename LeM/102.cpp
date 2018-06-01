a11c8c4c41387c435469479f1504367ae2978202
Fatnerd <quanfy98@gmail.com>
2018-06-01 23:41:52 +0800

    6.1 done

---
+++ LeM/102.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void travel(TreeNode *root, int level, vector<vector<int>>& result) {
+        if(root == nullptr) return;
+        if(level > result.size())
+            result.push_back(vector<int>());
+        result[level - 1].push_back(root->val);
+        travel(root->left, level+1, result);
+        travel(root->right, level+1, result);
+    }
+
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        travel(root, 1, result);
+        return result;
+    }
+};
+
+// runtime 6ms
